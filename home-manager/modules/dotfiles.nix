4695a5c3b405d18bfaaf8889bc17dae8983c156d
Ryan Denby <ryan.543@live.co.uk>
2021-08-27 16:16:46 +0100

    Update

--- home-manager/modules/dotfiles.nix
+++ home-manager/modules/dotfiles.nix
@@ -3,7 +3,7 @@
   home.file.".npmrc".source = config/dotfiles/.npmrc;
   home.file.".fehbg".source = config/dotfiles/.fehbg;
   home.file.".nix-channels".source = config/dotfiles/.nix-channels;
-  home.file.".zprofile".source = config/dotfiles/.zprofile;
+  home.file.".zprofile".source = config/shell/profile;
 
   # Config files
   xdg.configFile."git".source = config/git;
