ca83b36d69fb86db5676e343a648e24b75723bc1
Ryan Denby <ryan.543@live.co.uk>
2021-09-09 20:16:03 +0100

    Python packages to work with emacs elpy

--- home-manager/modules/nixos-desktop.nix
+++ home-manager/modules/nixos-desktop.nix
@@ -9,16 +9,22 @@
     gcc
     nodejs
     nodePackages.npm
-    # nodePackages.vls
     nodePackages.typescript-language-server
     nodePackages.vls
     nodePackages."@vue/cli"
     php
     haskell-language-server
     ghc
-    python38Packages.python-language-server
+    python39Packages.python-lsp-server
     python39Full
+    python39Packages.setuptools
+    python39Packages.ipython
     python39Packages.pylint
+    python39Packages.pip
+    python39Packages.flake8
+    python39Packages.black
+    python39Packages.virtualenv
+    pipenv
     nixfmt
     # Utils
     unzip
