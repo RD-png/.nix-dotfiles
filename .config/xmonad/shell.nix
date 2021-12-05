1f8c093fc285b27bc526ac1df16e4fd9775c19ad
Ryan Denby <ryan.543@live.co.uk>
2021-12-05 13:13:41 +0000

    Xmonad mod

--- .config/xmonad/shell.nix
+++ .config/xmonad/shell.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8104" }:
 let
   inherit (nixpkgs) pkgs;
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
