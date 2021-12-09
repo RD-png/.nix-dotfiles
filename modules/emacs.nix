a4e31e095bf3c9aa160cc097693db7575874f7f0
Ryan Denby <ryan.543@live.co.uk>
2021-12-09 21:01:23 +0000

    Emacs 29

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,15 +1,13 @@
-{ config, pkgs, inputs, ... }: {    
-    services.emacs.enable = true;
-    # services.emacs.package = pkgs.emacsGcc;
-    home.packages = with pkgs; [
-      # emacsGcc
-      mu
-      binutils
-      zstd
-      emacs-all-the-icons-fonts
-      rnix-lsp
-      gnutls
-      isync
-      ispell
-    ];  
+{ config, pkgs, inputs, ... }: {
+  services.emacs.enable = true;
+  home.packages = with pkgs; [
+    mu
+    binutils
+    zstd
+    emacs-all-the-icons-fonts
+    rnix-lsp
+    gnutls
+    isync
+    ispell
+  ];
 }
