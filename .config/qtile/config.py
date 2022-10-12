9787d1a5cd3acad4ac2a1395dc25a7bcb22c56cc
Ryan Denby <ryan.543@live.co.uk>
2022-10-12 21:33:11 +0100

    Fix qtile crash

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -173,18 +173,6 @@ colors = [
     ["#242831", "#242831"],  # super dark background
 ]
 
-def cus_battery():
-    if socket.gethostname() == "nixos-laptop":
-        return widget.Battery(
-                    foreground=colors[0],
-                    padding=3,
-                    charge_char="",
-                    discharge_char="",
-                    update_delay=15,
-                    format="{percent:.0%}{char}",
-                ),
-    return widget.TextBox(text="", padding=0, fontsize=0)
-
 screens = [
     Screen(
         top=bar.Bar(
@@ -231,7 +219,14 @@ screens = [
                     size_percent=50,
                 ),
                 widget.BatteryIcon(),
-                cus_battery(),
+                widget.Battery(
+                    foreground=colors[0],
+                    padding=3,
+                    charge_char="",
+                    discharge_char="",
+                    update_delay=15,
+                    format="{percent:.0%}{char}",
+                ),
                 widget.Sep(
                     linewidth=0,
                     padding=10,
