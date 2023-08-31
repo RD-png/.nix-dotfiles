264611642f2519de72567525499a318cacf1fafe
Ryan Denby <ryan.543@live.co.uk>
2023-08-31 21:53:31 +0100

    Disabled touchpad on laptop

--- hosts/nixos-laptop/default.nix
+++ hosts/nixos-laptop/default.nix
@@ -2,4 +2,14 @@
   imports = [ ./hardware-configuration.nix ./home.nix ];
 
   networking.hostName = "nixos-laptop";
+
+  services = {
+    xserver = {
+      libinput = {
+        touchpad = {
+          sendEventsMode = "disabled";
+        };
+      };
+    };
+  };
 }
